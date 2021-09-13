695c0837e5efb117b6bb94b7345629d925f6a6f4
ra-marukun <90334926+ra-marukun@users.noreply.github.com>
2021-09-14 00:17:57 +0900

    ABC218 解きなおし

--- contests/abc218/d/d.py
+++ contests/abc218/d/d.py
@@ -3,32 +3,49 @@ import sys
 
 ##ここに入力テキストを入れる
 _INPUT = """\
-6
-0 0
+7
 0 1
 1 0
-1 1
 2 0
 2 1
+2 2
+3 0
+3 2
 
 """
 sys.stdin = io.StringIO(_INPUT)
 
 ##ここ以降にコーディング
-from itertools import combinations
-
 n = int(input())
 xy = [map(int, input().split()) for _ in range(n)]
 X, Y = [list(i) for i in zip(*xy)]
 
-setX = list(set(X))
-xlist = [[] for _ in range(len(setX))]
+setX = set(X)
+setY = set(Y)
+lenX = len(setX)
+lenY = len(setY)
+xlist = [set() for _ in range(lenX)]
 
-for i in range(n):
-    idx = setX.index(X[i])
-    xlist[idx].append(Y[i])
+Dx = { v: i for i, v in enumerate(setX) }
+Dy = { v: i for i, v in enumerate(setY) }
 
-iterlist = [[] for _ in range(len(setX))]
-for i,x in enumerate(xlist):
-    iterlist[i] = list(combinations(x,2))
-print(iterlist)
+X = list(map(lambda v: Dx[v], X))
+Y = list(map(lambda v: Dy[v], Y))
+
+
+xlist = {x: set() for x in X}
+
+for i in range(len(X)):
+    xlist[X[i]].add(Y[i])
+
+ans = 0
+for x_left in range(lenX-1):
+    for x_right in range(x_left+1, lenX):
+        xl = xlist[x_left]
+        xr = xlist[x_right]
+        for yl in xl:
+            for yr in xr:
+                if yl < yr:                
+                    if (yr in xl) & (yl in xr):
+                        ans += 1
+print(ans)
