6d9cdd0b153b0c4bfaaf762371d4a2476f90091b
ra-marukun <90334926+ra-marukun@users.noreply.github.com>
2021-09-16 23:28:16 +0900

    from surface

--- other_problems/typical90/012/012.py
+++ other_problems/typical90/012/012.py
@@ -3,9 +3,21 @@ import sys
 
 ##ここに入力テキストを入れる
 _INPUT = """\
+3 3
+10
+1 2 2
+1 1 1
+2 1 1 2 2
+1 3 2
+2 1 1 2 2
+2 2 2 3 2
+1 2 3
+1 2 1
+2 1 1 2 2
+2 1 1 3 3
 
 """
 sys.stdin = io.StringIO(_INPUT)
 
 ##ここ以降にコーディング
-
+h, w = map(int, input().split())
